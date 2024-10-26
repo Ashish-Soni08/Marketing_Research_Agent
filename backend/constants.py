d0fb4f23f029f3e92d13d712fa5d20c7bfd7c2cb
Ashish Soni <ashish.soni2091@gmail.com>
2024-10-26 00:10:55 +0000

    model names

---
+++ backend/constants.py
@@ -0,0 +1,9 @@
+# MODEL HOSTED BY GROQ
+GUARDRAIL_MODEL: str = "llama-guard-3-8b"
+
+# MODELS HOSTED BY NVIDIA(NIMS) 
+LLM_MODEL: str ="meta/llama-3.2-1b-instruct"
+
+EMBEDDING_MODEL: str ="nvidia/llama-3.2-nv-embedqa-1b-v1"
+
+RERANK_MODEL: str ="nvidia/llama-3.2-nv-embedqa-1b-v1"
